81f0907ee118606a61ca0980a638d42653840222
Felix Borchers <h1f1x@users.noreply.github.com>
2024-04-29 09:20:41 +0200

    Add led code for blinking more sophisticated

---
+++ led.py
@@ -0,0 +1,13 @@
+# from https://projects.raspberrypi.org/en/projects/getting-started-with-the-pico/5
+
+from machine import Pin, Timer
+
+led = Pin(13, Pin.OUT)
+timer = Timer()
+
+
+def blink(timer):
+    led.toggle()
+
+
+timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
